72a40125172173251a5a1c86e31af27edda652a5
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-02-11 12:07:29 +0300

    font fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
     case text_length
     when (0..99) then '36px'
     when (100..199) then '32px'
-    when (200..299) then '28px'
-    when (300..499) then '24px'
-    when (500..999) then '22px'
-    when (1000..1499) then '18px'
+    when (200..299) then '26px'
+    when (300..499) then '20px'
+    when (500..999) then '18px'
+    when (1000..1499) then '16px'
     else
       '14px'
     end
