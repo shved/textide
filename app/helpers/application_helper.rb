7e23581c2f7a9e426a9d9e0a0fe130fbe30b05c8
Vitaly Shvedchenko <vitaly.shvedchenko@uchi.ru>
2016-02-11 11:52:09 +0300

    style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def proper_font_size(text_length)
+    case text_length
+    when (0..99) then '36px'
+    when (100..199) then '32px'
+    when (200..299) then '28px'
+    when (300..499) then '24px'
+    when (500..999) then '22px'
+    when (1000..1499) then '18px'
+    else
+      '14px'
+    end
+  end
 end
